f431faa584eaaa063f1ae69f822da01bbc500f6b
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-08 01:30:43 -0500

    Log a message based on command-line arguments passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const numberArg = process.argv.slice(2).length;
+console.log(
+    numberArg === 0
+    ? 'No argument'
+    : numberArg === 1
+    ? 'Argument found'
+    : 'Arguments found'
+);
