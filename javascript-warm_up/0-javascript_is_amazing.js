346a9d5aee7759032d9a9cfaa5f6249d1815d09f
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-08 00:57:04 -0500

    add javascript-warm_up folder

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+function printContent(myVar) { return myVar };
+
+const myVar = 'JavaScript is amazing';
+console.log(printContent(myVar));
---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function printThree() {
+    console.log('C is fun');
+    console.log('Python is cool');
+    console.log('JavaScript is amazing');
+}
+
+printThree();
