cff1f83153ca4188043ca0f69b31f2a8109e7a1e
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-08 01:11:56 -0500

    Log three lines of codes

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-function printThree() {
-    console.log('C is fun');
-    console.log('Python is cool');
-    console.log('JavaScript is amazing');
-}
-
-printThree();
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
